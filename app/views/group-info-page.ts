17ff8bb7e2734313b60c518bb1b1d33cfedc7b88
vakrilov <alexander.vakrilov@telerik.com>
2015-10-01 15:33:08 +0300

    add missing files

---
+++ app/views/group-info-page.ts
@@ -0,0 +1,20 @@
+import pages = require("ui/page");
+import view = require("ui/core/view");
+import gestures = require("ui/gestures");
+import observable = require("data/observable");
+import navigator = require("../common/navigator");
+
+// Event handler for Page "navigatedTo" event attached in details-page.xml
+export function pageNavigatingTo(args: pages.NavigatedData) {
+    // Get the event sender
+    var page = <pages.Page>args.object;
+    page.bindingContext = args.context;
+}
+
+export function navigateBack(args: gestures.GestureEventData) {
+    navigator.navigateBack();
+}
+
+export function openLink(args:  observable.EventData) {
+    navigator.openLink(args.object);
+}
