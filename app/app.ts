f2659b4b496b500582c7eeac5d4730fbcaec8cf8
Panayot Cankov <Panayot.Cankov@telerik.com>
2015-10-05 14:14:51 +0300

    Support only portrait orientation. Set StatusBar background to black and foreground to white.

--- app/app.ts
+++ app/app.ts
@@ -9,6 +9,9 @@ if (application.ios) {
     application.on("launch", args => {
         // TODO: It would be nice if this was ios-specific property on the action bar and static property on application.ios.
         UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.UIStatusBarStyleLightContent;
+        setTimeout(() => {
+            UIApplication.sharedApplication().keyWindow.backgroundColor = UIColor.blackColor();
+        }, 1);
     });
 }
 
