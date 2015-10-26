9baca5cb6e6f7830ade6929b0d5928298c40f074
vakrilov <alexander.vakrilov@telerik.com>
2015-10-26 11:14:16 +0200

    Navigation fix - ios

--- app/common/navigator.ts
+++ app/common/navigator.ts
@@ -5,6 +5,9 @@ import frame = require("ui/frame");
 import viewModule = require("ui/core/view");
 import platform = require("platform");
 
+var isIOS: boolean = platform.device.os === platform.platformNames.ios;
+var isAndroid: boolean = platform.device.os === platform.platformNames.android;
+
 export function navigateToExampleGroup(context: groupVM.GroupPageViewModel) {
     frame.topmost().navigate({
         animated: true,
@@ -19,6 +22,14 @@ export function navigateToExample(example: examplesVM.Example) {
         example: example
     }
 
+    if (isIOS) {
+        frame.topmost().navigate({
+            animated: false,
+            context: new exampleInfoPageVM.ExampleInfoPageViewModel(navContext.example),
+            moduleName: "views/example-info-page",
+        });
+    }
+
     frame.topmost().navigate({
         animated: true,
         moduleName: example.path,
@@ -28,10 +39,10 @@ export function navigateToExample(example: examplesVM.Example) {
 
 export function navigateBackFromExampe(context: exampleInfoPageVM.ExampleNavigationContext) {
     frame.goBack();
-    
-    if (context && context.shouldNavigateToInfoOnBack) {
+
+    if (isAndroid && context && context.shouldNavigateToInfoOnBack) {
         var infoContext = new exampleInfoPageVM.ExampleInfoPageViewModel(context.example);
-        
+
         frame.topmost().navigate({
             animated: false,
             context: infoContext,
@@ -71,14 +82,14 @@ export function navigateBack() {
 export function openLink(view: any) {
     var url = view.tag;
     if (url) {
-        if (platform.device.os === platform.platformNames.ios) {
+        if (isIOS) {
             var nsUrl = NSURL.URLWithString(url);
             var sharedApp = UIApplication.sharedApplication();
             if (sharedApp.canOpenURL(nsUrl)) {
                 sharedApp.openURL(nsUrl);
             }
         }
-        else if (platform.device.os === platform.platformNames.android) {
+        else if (isAndroid) {
             var intent = new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse(url));
             var activity = frame.topmost().android.activity;
             activity.startActivity(android.content.Intent.createChooser(intent, "share"));
