9f7b50dec1a31d3cb6efb49d8d6b24da87504a08
vakrilov <alexander.vakrilov@telerik.com>
2015-10-27 17:58:35 +0200

    splash screen

--- app/app.ts
+++ app/app.ts
@@ -6,6 +6,20 @@ import exampleBase = require("./examples/example-base-page");
 import application = require("application");
 import prof = require("./common/profiling");
 
+if(application.android) {
+    application.onLaunch = function (intent) {
+        console.log("onLaunch");
+
+        application.android.onActivityStarted = function (activity) {
+            console.log("onStarted");
+            var window = activity.getWindow();
+            if (window) {
+                window.setBackgroundDrawable(null);
+            }
+        }
+    }
+}
+
 if (application.ios) {
     application.on("launch", args => {
         // TODO: It would be nice if this was ios-specific property on the action bar and static property on application.ios.
