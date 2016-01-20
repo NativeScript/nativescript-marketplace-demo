54843bd41f14de255a1792e7f73c099ce3d823d4
Deyan Ginev <deyan.ginev@telerik.com>
2016-01-20 16:08:26 +0200

    Fixed issue with layouts example

--- app/examples/listview/layouts/listview-layouts.ts
+++ app/examples/listview/layouts/listview-layouts.ts
@@ -29,19 +29,15 @@ export function pageNavigatedFrom(args: any) {
 
 export function onLinearLayoutTap(args: any) {
 	var linearLayout = new listViewModule.ListViewLinearLayout();
-	linearLayout.ios.dynamicItemSize = false;
-	linearLayout.itemHeight = 250;
 	listView.listViewLayout = linearLayout;
 	viewModel.isLinearActive = true;
 	viewModel.isWrapActive = false;
-	
 }
 
 export function onGridLayoutTap(args: any) {
 	var gridLayout = new listViewModule.ListViewGridLayout();
-	gridLayout.dynamicItemSize = false;
 	gridLayout.spanCount = 2;
-	gridLayout.itemHeight = 250;
+	gridLayout.itemHeight = "250";
 	listView.listViewLayout = gridLayout;
 	viewModel.isLinearActive = false;
 	viewModel.isWrapActive = true;
