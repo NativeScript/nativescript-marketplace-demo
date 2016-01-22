125ce608598efd9c6946b6ada1576850a84e694e
Deyan Ginev <deyan.ginev@telerik.com>
2016-01-22 17:11:19 +0200

    Summary for selection items

--- app/examples/listview/selection/main-page-model.ts
+++ app/examples/listview/selection/main-page-model.ts
@@ -11,10 +11,11 @@ var DELTA = 0.1;
 
 class BlogPostItemData extends observableModule.Observable {
 
-    constructor(title: string, content: string, fav: boolean, id: number) {
+    constructor(title: string, content: string, summary: string, fav: boolean, id: number) {
         super();
         this.Title = title;
         this.Content = content;
+        this.Summary = summary;
         this.IsFavourite = fav;
         this.ID = id;
         this.reorderActive = false;
@@ -68,6 +69,14 @@ class BlogPostItemData extends observableModule.Observable {
     set IsFavourite(value: boolean) {
         this.set("_isFavourite", value);
     }
+
+    get Summary() {
+        return this.get("_summary");
+    }
+
+    set Summary(value: string) {
+        this.set("_summary", value);
+    }
 }
 
 export class ListView_ViewModel extends observableModule.Observable {
@@ -80,30 +89,37 @@ export class ListView_ViewModel extends observableModule.Observable {
         this._itemsBackup.push(new BlogPostItemData(
             "Master the Essentials of UI Test Automation: Chapter One",
             "Chapter One: Introduction The goal of this series is to help you understand the right questions to ask of you, your team and your organization. There won't be any Best Practices; there won't be any silver bullets. What we hope is to convey the right information to help you get started on the right foot and get through some of the most common problems teams hit when starting out with UI test automation.",
+            "Chapter One: Introduction The goal of this series is to help you understand the right questions to ask of you, your team and your organization. There won't be any Best Practices; there won't be any silver bullets.",
             false, 1));
         this._itemsBackup.push(new BlogPostItemData(
             "Send Data to Apple Watch with Core Data and Telerik UI for iOS in Swift",
             "The Apple Watch has been a long rumored device which finally appeared in September, followed by a Watch SDK, called WatchKit, in November. The introduction of the SDK maybe raised more questions than it answered, and we like everybody else are looking into the future for answers from Apple. One such question is: how can I send data, larger than what is allowed for a push notification, from the iPhone to the Watch?",
+            "The Apple Watch has been a long rumored device which finally appeared in September, followed by a Watch SDK, called WatchKit, in November.",
             true, 2));
         this._itemsBackup.push(new BlogPostItemData(
             "6 Key Steps to Successful Agile Testing Projects",
+            "Application teams are continuously adopting agile software techniques as the principal method of building applications. Agile methodologies, such as Scrum, Extreme Programming, Feature-Driven Development and Test-Driven Development offer the ability to iteratively develop applications.",
             "Application teams are continuously adopting agile software techniques as the principal method of building applications. Agile methodologies, such as Scrum, Extreme Programming, Feature-Driven Development and Test-Driven Development offer the ability to iteratively develop applications.",
             true, 3));
         this._itemsBackup.push(new BlogPostItemData(
             "Telerik UI for Xamarin Now Supports Xamarin.Forms 1.3.0",
             "Recently, the Xamarin guys came with a so to say major version of Xamarin.Forms, 1.3.0. This version brought enhancements as well as some changes that required some internal changes on our side. We are well aware that you will want to benefit from the Xamarin.Forms improvements immediately, while still using Telerik UI for Xamarin, so I am happy to announce the availability of a new UI for Xamarin version supporting Xamarin.Forms 1.3.0. Feel free to NuGet-update your Telerik UI for Xamarin projects to Xamarin.Forms 1.3.0.",
+            "Recently, the Xamarin guys came with a so to say major version of Xamarin.Forms, 1.3.0. This version brought enhancements as well as some changes that required some internal changes on our side.",
             false, 4));
         this._itemsBackup.push(new BlogPostItemData(
             "Increase Your Mobile App Engagement. Become Part of the Web of Apps.",
             "Mobile developers are facing some severe limitations when it comes to app distribution: app content is almost invisible to browser search, app-to-app connections are scarce, and app updates need to go through a tedious re-submission process on the relevant marketplace. How can Google App Indexing, deep linking and Google Tag Manager for mobile apps help in the process of overcoming these limitations?",
+            "Mobile developers are facing some severe limitations when it comes to app distribution: app content is almost invisible to browser search, app-to-app connections are scarce, and app updates need to go through a tedious re-submission process on the relevant marketplace.",
             true, 5));
         this._itemsBackup.push(new BlogPostItemData(
             "Building a Seismograph App with CoreMotion, Swift and Telerik UI for iOS",
             "Data visualizations are important, especially on small screen areas, where Excel files or other tables are difficult to read and comprehend. Visualizations are even more important when you add the various sensors that an iPhone device offers, not to mention the different certified third-party devices. Today, I will show you how you can set up the Telerik Chart for iOS to live-stream data coming from the accelerometer sensor using the CoreMotion API.",
+            "Data visualizations are important, especially on small screen areas, where Excel files or other tables are difficult to read and comprehend.",
             false, 6));
         this._itemsBackup.push(new BlogPostItemData(
             "Application Performance Monitoring with the Crittercism Cordova Plugin",
             "As hybrid mobile apps scale to massive amounts of users and tremendous amount of data, developers need to monitor and trace the app’s performance. Crittercism is the world's first mobile application performance management (mAPM) solution, offering both error monitoring and service monitoring solutions. The Crittercism service monitors every aspect of mobile app performance, allowing Developers and IT Operations to deliver high performing, highly reliable, highly available mobile apps.",
+            "As hybrid mobile apps scale to massive amounts of users and tremendous amount of data, developers need to monitor and trace the app’s performance.",
             false, 7));
     }
 
