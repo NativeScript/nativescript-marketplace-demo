d810f14d786d99a6dfc7ed7487592712e5607953
Deyan Ginev <deyan.ginev@telerik.com>
2016-02-23 11:24:28 +0200

    Updated fresco plugin. Fixed issue with layouts example

--- app/examples/listview/layouts/listview-layouts.ts
+++ app/examples/listview/layouts/listview-layouts.ts
@@ -8,39 +8,41 @@ var viewModel: model.ListViewLayoutsModel = new model.ListViewLayoutsModel();
 var listView;
 
 export function pageNavigatingTo(args: any) {
-	var page = args.object;
-	page.bindingContext = viewModel;
-	listView = page.getViewById("list-view");
-	viewModel.isLinearActive = true;
-	viewModel.isWrapActive = false;
+    var page = args.object;
+    page.bindingContext = viewModel;
+    listView = page.getViewById("list-view");
+    viewModel.isLinearActive = true;
+    viewModel.isWrapActive = false;
 
-	if (applicationModule.android && android.os.Build.VERSION > 18) {
-		var window = applicationModule.android.foregroundActivity.getWindow();
-		window.addFlags(android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-	}
+    if (applicationModule.android && android.os.Build.VERSION > 18) {
+        var window = applicationModule.android.foregroundActivity.getWindow();
+        window.addFlags(android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
 }
 
 export function pageNavigatedFrom(args: any) {
-	if (applicationModule.android && android.os.Build.VERSION > 18) {
-		var window = applicationModule.android.foregroundActivity.getWindow();
-		window.clearFlags(android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-	}
+    if (applicationModule.android && android.os.Build.VERSION > 18) {
+        var window = applicationModule.android.foregroundActivity.getWindow();
+        window.clearFlags(android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
 }
 
 export function onLinearLayoutTap(args: any) {
-	var linearLayout = new listViewModule.ListViewLinearLayout();
+    var linearLayout = new listViewModule.ListViewLinearLayout();
     linearLayout.itemHeight = 250;
-	listView.listViewLayout = linearLayout;
-	viewModel.isLinearActive = true;
-    linearLayout.ios.dynamicItemSize = false;
-	viewModel.isWrapActive = false;
+    listView.listViewLayout = linearLayout;
+    viewModel.isLinearActive = true;
+    if (linearLayout.ios) {
+        linearLayout.ios.dynamicItemSize = false;
+    }
+    viewModel.isWrapActive = false;
 }
 
 export function onGridLayoutTap(args: any) {
-	var gridLayout = new listViewModule.ListViewGridLayout();
-	gridLayout.spanCount = 2;
-	gridLayout.itemHeight = 250;
-	listView.listViewLayout = gridLayout;
-	viewModel.isLinearActive = false;
-	viewModel.isWrapActive = true;
+    var gridLayout = new listViewModule.ListViewGridLayout();
+    gridLayout.spanCount = 2;
+    gridLayout.itemHeight = 250;
+    listView.listViewLayout = gridLayout;
+    viewModel.isLinearActive = false;
+    viewModel.isWrapActive = true;
 }
