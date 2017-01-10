36549fd29cf0087872df46c30bdc732637f20b9c
tgpetrov <Todor.Petrov@telerik.com>
2017-01-10 10:21:08 +0200

    added a dataform example

--- app/ios-dummy.d.ts
+++ app/ios-dummy.d.ts
@@ -8,6 +8,7 @@ declare var NSURL: any;
 declare type UIColor = any;
 declare var UIColor: any;
 declare type UIButton = any;
+declare var UIButton: any;
 declare type UIImage = any;
 declare var UIImage: any;
 declare type UIImageView = any;
@@ -30,7 +31,6 @@ declare type UITableView = any;
 declare type UITableViewCell = any;
 declare type CGRect = any;
 declare type CGPoint = any;
-declare type UIView = any;
 declare function CACurrentMediaTime(): number;
 declare type NSMutableArray = any;
 declare var NSMutableArray: any;
@@ -38,6 +38,42 @@ declare type CAGradientLayer = any;
 declare var CAGradientLayer: any;
 declare function CGPointMake(x: number, y: number): CGPoint;
 declare var UITableViewCellSeparatorStyle: any;
+declare var UIFont: any;
+declare var UIButtonType: any;
+declare type UIPickerView = any;
+declare var UIControlState: any;
+declare var UIControlEvents: any;
+declare var UIControlContentHorizontalAlignment: any;
+declare type NSInteger = any;
+declare type NSString = any;
+declare class NSObject {
+	static new(): NSObject;
+	static alloc(): NSObject;
+	init(): NSObject;
+}
+declare class UIView {
+	static class(): any;
+}
+declare class NSDictionary {
+	static new(): NSDictionary;
+	static alloc(): NSDictionary;
+	initWithObjectsForKeys(objects: any, keys: any): NSDictionary;
+}
+declare class NSAttributedString {
+	static new(): NSAttributedString;
+	static alloc(): NSAttributedString;
+	initWithStringAttributes(string: any, attributes: any): NSAttributedString;
+}
+interface UIPickerViewDelegate {
+}
+declare var UIPickerViewDelegate: any;
+declare module interop {
+    var types: {
+        "void": any;
+    }
+}
 
 // telerik-ui
 declare var TKSideDrawerShadowMode: any;
+declare var TKStroke: any;
+declare type TKDataFormPickerViewEditor = any;
