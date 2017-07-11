becf8614f36f1c60bd36bf778e58d40f8011bae6
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2017-07-11 19:46:10 +0300

    refactor: add firebase plugin to snapshotted chunk (#273)

--- app/vendor.ts
+++ app/vendor.ts
@@ -2,6 +2,8 @@ require("./vendor-platform");
 require("bundle-entry-points");
 require("highlight.js");
 
+require("nativescript-plugin-firebase");
+
 require("nativescript-telerik-ui-pro/chart");
 require("nativescript-telerik-ui-pro/dataform");
 require("nativescript-telerik-ui-pro/listview");
