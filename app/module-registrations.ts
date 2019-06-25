63cc0efeb6131f7e52ec6e7eee4e22e3633e01df
tbozhikov <teodor.bozhikov@gmail.com>
2019-06-25 19:12:04 +0300

    fix: make app buildable with next (6.0) CLI

--- app/module-registrations.ts
+++ app/module-registrations.ts
@@ -2,4 +2,5 @@
 if (global.TNS_WEBPACK) {
     // Register nativescript-ui-chart since it is loaded dynamically 
     global.registerModule("nativescript-ui-chart", function () { return require("nativescript-ui-chart"); });
+    global.registerModule("nativescript-ui-listview", function () { return require("nativescript-ui-listview"); });
 }
