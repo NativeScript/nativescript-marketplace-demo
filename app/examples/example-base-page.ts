94ec3dba74361e9a8a6470df3178115a0a2b3a34
vakrilov <alexander.vakrilov@telerik.com>
2019-06-27 16:53:17 +0300

    refactor: example-menu cleanup

--- app/examples/example-base-page.ts
+++ app/examples/example-base-page.ts
@@ -1,12 +1,9 @@
-import { Page } from "tns-core-modules/ui/page";
+import { Page, Observable, ViewBase } from "tns-core-modules/ui/page";
 // import { isAndroid } from "tns-core-modules/platform";
 import * as prof from "../common/profiling";
 import * as builder from "tns-core-modules/ui/builder";
 import { View } from "tns-core-modules/ui/core/view"
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
-
-// var OVERLAY_ELEVATION = 12;
-// var CURVE = isAndroid ? new android.view.animation.AccelerateDecelerateInterpolator() : UIViewAnimationCurve.UIViewAnimationCurveEaseInOut;
 
 export class ExamplePage extends Page {
 
@@ -39,14 +36,15 @@ export class ExamplePage extends Page {
         // prof.stopCPUProfile("example");
         prof.stop("example");
 
-        this.actionBar.actionItems.getItems().forEach(item => {
-            if ((<any>item).id === "exampleMenuButton") {
-                item.on("tap", () => {
-                    // TODO: Toggle instead
-                    this.sideDrawer.gesturesEnabled = true;
-                    this.sideDrawer.showDrawer();
-                });
+        this.actionBar.actionItems.getItems().forEach((item: ViewBase) => {
+            if (item.id === "exampleMenuButton") {
+                item.off("tap", this.toggleDrawer, this);
+                item.on("tap", this.toggleDrawer, this);
             }
         });
+    }
+
+    private toggleDrawer() {
+        this.sideDrawer.toggleDrawerState();
     }
 }
