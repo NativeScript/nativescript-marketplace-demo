cfbd65ba8ebfe69f732087bd1113476a70a981eb
tbozhikov <teodor.bozhikov@gmail.com>
2019-07-22 10:26:33 +0300

    fix: add back the examples code to webpack copy command

--- webpack.config.js
+++ webpack.config.js
@@ -234,6 +234,8 @@ module.exports = env => {
             new CleanWebpackPlugin(itemsToClean, { verbose: !!verbose }),
             // Copy assets to out dir. Add your own globs as needed.
             new CopyWebpackPlugin([
+                { from: { glob: "examples/**" } },
+                { from: { glob: "views/**" } },
                 { from: { glob: "fonts/**" } },
                 { from: { glob: "**/*.jpg" } },
                 { from: { glob: "**/*.png" } },
